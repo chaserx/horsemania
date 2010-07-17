04264d02b4a812e2d8b0508356a53378022c4b5e
Chase Southard <chase.southard@gmail.com>
2010-07-17 19:55:59 -0400

    minor tweaks to location model trying to understand how polypaperlip works

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,9 @@
 class Location < ActiveRecord::Base
   # hackers love mass assignment
-  attr_accessible :title, :desc, :lat, :lon, :artist, :artist_location, :sponsor
+  attr_accessible :title, :desc, :lat, :lon, :artist, :artist_location, :sponsor, :data, :asset
   
   # not sure about this
-  has_many :photos
+  #has_many :photos
   
   # for paperclippolymorph plugin
   # how to use s3?
