37d94f1095cf46fc6ab7203e2f256b6f92581413
Chase Southard <chase.southard@gmail.com>
2010-07-17 18:49:06 -0400

    first step into storing photos

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :title, :desc, :lat, :lon, :artist, :artist_location, :sponsor
+  
+  has_many :photos
+  
+  acts_as_polymorphic_paperclip 
 end
