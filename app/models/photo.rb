de5368bf56daab4f00e607613e6e2c58ef2293f8
Chase Southard <chase.southard@gmail.com>
2010-07-24 18:02:20 -0400

    added more styles to photo upload attachment for paperclip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   attr_accessible :title
   attr_protected :upload_file_name, :upload_content_type, :upload_size
   
-  has_attached_file :upload, :styles => { :small => "150x150>" },
+  has_attached_file :upload, :styles => { :tiny => "64x64#", :small => "150x150>", :medium => "630x630>", :large => "1024x1024>"  },
                     :url  => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
