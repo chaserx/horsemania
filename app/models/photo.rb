24d0eb58076ef3b417a5e5bda1caf2462fa2beae
Chase Southard <chase.southard@gmail.com>
2010-07-25 21:24:19 -0400

    fixed my fuckup i created earlier

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :title
-  attr_protected :upload_file_name, :upload_content_type, :upload_size
+  attr_accessible :title, :upload
+  attr_accessible :upload_file_name, :upload_content_type, :upload_size
   
   has_attached_file :upload, :styles => { :tiny => "64x64#", :small => "150x150>", :medium => "630x630>", :large => "1024x1024>"  },
-                    :url  => "/assets/products/:id/:style/:basename.:extension",
+                    :url  => "/assets/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/:id/:style/:basename.:extension"
 
   validates_attachment_presence :upload
